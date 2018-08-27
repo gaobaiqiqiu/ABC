d834ee957fa373b8f602bc199546f67fa78d8b7e
gaobaiqiqiu <294002670@qq.com>
2018-08-27 16:22:54 +0800

    18.8.27更新

--- js/ting.js
+++ js/ting.js
@@ -3,11 +3,12 @@ var listZi = {a:['Aa'],b:['Bb']}
 for( var item in fileListTing ){  //fileList文件列表
     var imgList = fileListTing[item];  //图片列表
     var listP = pList[item];
+    console.log(listP)
     var letterList = {};
     for(var i=0;i<imgList.length;i++){
         var str = imgList[i];
         var className = str.substring(0,str.length-4);  //截取文件名获取类名
-        letterList[i] = $($('#template').html().replace('$url$','http://www.dadpat.com/apk/ABC/ting/'+item+'/'+str).replace('$imgClass$',className).replace('$divClass$',className +'D').replace('$content$',listP[i]).replace('$pClass$',className +'P'));
+        letterList[i] = $($('#template').html().replace('$url$','http://www.dadpat.com/apk/ABC/ting/'+item+'/'+str).replace('$class$',className).replace('$content$',listP[i]));
         $('#swiper-container1 .swiper-wrapper').append(letterList[i])
     }
 }
@@ -91,6 +92,7 @@ var swiper1 = new Swiper('#swiper-container1', {
     },
     on:{
         slideChangeTransitionStart:function(){
+            
             // 获取当前图片的transform值
             var transForm = $('#swiper-container1 .swiper-scrollbar-drag')[0].style.transform;
             // 获取当前图片的transform的X值
@@ -102,60 +104,7 @@ var swiper1 = new Swiper('#swiper-container1', {
             var widthAll = $('.slide')[0].width;
             $('#swiper-container1 .swiper-pagination').css('width',(widthAll - moveTransFormX) +'px')
             // 星星上的P标签
-            var PXX = $('.swiper-slide-active p')[0].innerText;
-            if(PXX == 'Aa'){
-                $('.progress p')[0].innerText = 'Aa';
-            }else if(PXX == 'Bb'){
-                $('.progress p')[0].innerText = 'Bb';
-            }else if(PXX == 'Cc'){
-                $('.progress p')[0].innerText = 'Cc';
-            }else if(PXX == 'Dd'){
-                $('.progress p')[0].innerText = 'Dd';
-            }else if(PXX == 'Ee'){
-                $('.progress p')[0].innerText = 'Ee';
-            }else if(PXX == 'Ff'){
-                $('.progress p')[0].innerText = 'Ff';
-            }else if(PXX == 'Gg'){
-                $('.progress p')[0].innerText = 'Gg';
-            }else if(PXX == 'Hh'){
-                $('.progress p')[0].innerText = 'Hh';
-            }else if(PXX == 'Ii'){
-                $('.progress p')[0].innerText = 'Ii';
-            }else if(PXX == 'Jj'){
-                $('.progress p')[0].innerText = 'Jj';
-            }else if(PXX == 'Kk'){
-                $('.progress p')[0].innerText = 'Kk';
-            }else if(PXX == 'Ll'){
-                $('.progress p')[0].innerText = 'Ll';
-            }else if(PXX == 'Mm'){
-                $('.progress p')[0].innerText = 'Mm';
-            }else if(PXX == 'Nn'){
-                $('.progress p')[0].innerText = 'Nn';
-            }else if(PXX == 'Oo'){
-                $('.progress p')[0].innerText = 'Oo';
-            }else if(PXX == 'Pp'){
-                $('.progress p')[0].innerText = 'Pp';
-            }else if(PXX == 'Qq'){
-                $('.progress p')[0].innerText = 'Qq';
-            }else if(PXX == 'Rr'){
-                $('.progress p')[0].innerText = 'Rr';
-            }else if(PXX == 'Ss'){
-                $('.progress p')[0].innerText = 'Ss';
-            }else if(PXX == 'Tt'){
-                $('.progress p')[0].innerText = 'Tt';
-            }else if(PXX == 'Uu'){
-                $('.progress p')[0].innerText = 'Uu';
-            }else if(PXX == 'Vv'){
-                $('.progress p')[0].innerText = 'Vv';
-            }else if(PXX == 'Ww'){
-                $('.progress p')[0].innerText = 'Ww';
-            }else if(PXX == 'Xx'){
-                $('.progress p')[0].innerText = 'Xx';
-            }else if(PXX == 'Yy'){
-                $('.progress p')[0].innerText = 'Yy';
-            }else if(PXX == 'Zz'){
-                $('.progress p')[0].innerText = 'Zz';
-            }
+            $('.progress p')[0].innerText = $('.swiper-slide-active p')[0].innerText;
 
             $(".xiaLa img").attr('src','image/2xiala.png'); 
             //隐藏卡片列表
